689ef10e342dee4802d5ec04a44f0b50ffb62831
atl043 <atl043@ucsd.edu>
2019-02-07 09:18:28 -0800

    finish lab5

--- routes/add.js
+++ routes/add.js
@@ -1,12 +1,13 @@
 var data = require("../data.json");
 
-exports.addFriend = function (req, res) {
-	var newFriend = {
-		"name": req.query.name,
-		"imageURL": 'http://lorempixel.com/400/400/people',
-		"description": req.query.description
-	};
+exports.addFriend = function(req, res) { 
+  var newFriend = {
+    "name": req.query.name,
+    "description": req.query.description,
+    "imageURL": 'http://lorempixel.com/400/400/people'
+  };
 
 	data['friends'].push(newFriend);
-	res.render('add', data);
+
+  res.render('add', data);
 };
